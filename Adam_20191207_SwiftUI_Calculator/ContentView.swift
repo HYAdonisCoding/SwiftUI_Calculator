97b7a998f0451ef92e56d6eafffd4343af0e9473
Adam <296786475@qq.com>
2019-12-07 15:51:03 +0800

    Initial Commit

---
+++ Adam_20191207_SwiftUI_Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adam_20191207_SwiftUI_Calculator
+//
+//  Created by Adam on 2019/12/7.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
