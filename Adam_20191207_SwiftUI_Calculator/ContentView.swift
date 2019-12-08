f71eee43a0252e35eacea75ca6ac1272438895d6
Adam <296786475@qq.com>
2019-12-08 10:22:37 +0800

    finish UI

--- Adam_20191207_SwiftUI_Calculator/ContentView.swift
+++ Adam_20191207_SwiftUI_Calculator/ContentView.swift
@@ -12,6 +12,7 @@ struct CalculatorButtonPad: View {
     let pad: [[CalculatorButtonItem]] = [
         [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
     [.digit(7), .digit(8), .digit(9), .op(.mutiply)],
+    [.digit(4), .digit(5), .digit(6), .op(.mins)],
     [.digit(1), .digit(2), .digit(3), .op(.plus)],
     [.digit(0), .dot, .op(.equal)]]
     var body: some View {
@@ -26,7 +27,20 @@ struct CalculatorButtonPad: View {
 struct ContentView: View {
     
     var body: some View {
-        CalculatorButtonPad()
+        VStack(spacing: 12) {
+            Spacer()
+            Text("0")
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24)
+                .lineLimit(1)
+                .frame(minWidth: 0,
+                       maxWidth: .infinity,
+                       alignment: .trailing)
+            CalculatorButtonPad()
+                .padding(.bottom)
+        }
+        
         
     }
 }
