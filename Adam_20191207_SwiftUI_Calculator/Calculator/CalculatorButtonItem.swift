d690b04bb4c1db74f87adc9cd5569a46f897f91b
Adam <296786475@qq.com>
2019-12-08 10:10:10 +0800

    CalculatorButtonPad

---
+++ Adam_20191207_SwiftUI_Calculator/Calculator/CalculatorButtonItem.swift
@@ -0,0 +1,57 @@
+//
+//  CalculatorButtonItem.swift
+//  Adam_20191207_SwiftUI_Calculator
+//
+//  Created by Adam on 2019/12/7.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+/// 计算器按钮model
+enum CalculatorButtonItem {
+    enum Op: String {
+        case plus = "+"
+        case mins = "-"
+        case divide = "÷"
+        case mutiply = "x"
+        case equal = "="
+    }
+    enum Command: String {
+        case clear = "AC"
+        case flip = "+/-"
+        case percent = "%"
+    }
+    case digit(Int)
+    case dot
+    case op(Op)
+    case command(Command)
+}
+
+extension CalculatorButtonItem {
+    var title: String {
+        switch self {
+        case .digit(let value): return String(value)
+        case .dot: return "."
+        case .op(let op): return op.rawValue
+        case .command(let command): return command.rawValue
+        }
+    }
+    var size: CGSize {
+        if case .digit(let value) = self, value == 0 {
+            return CGSize(width: 88 * 2 + 8, height: 88)
+        }
+        return CGSize(width: 88, height: 88)
+    }
+    
+    var backgroundColorName: String {
+        switch self {
+        case .digit, .dot: return "digitBckground"
+        case .op: return "digitBckground"
+        case .command: return "digitBckground"
+        }
+    }
+}
+
+extension CalculatorButtonItem: Hashable {}
--- Adam_20191207_SwiftUI_Calculator/ContentView.swift
+++ Adam_20191207_SwiftUI_Calculator/ContentView.swift
@@ -8,18 +8,25 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
+    [.digit(7), .digit(8), .digit(9), .op(.mutiply)],
+    [.digit(1), .digit(2), .digit(3), .op(.plus)],
+    [.digit(0), .dot, .op(.equal)]]
     var body: some View {
-        Button.init(action: {
-            print("Button : + ")
-        }) {
-            Text("+")
-            .font(.system(size: 38))
-            .foregroundColor(.white)
-            .frame(width: 88, height: 88, alignment: .center)
-            .background(Color.orange)
-            .cornerRadius(44)
+        VStack(spacing: 8) {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
         }
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        CalculatorButtonPad()
         
     }
 }
@@ -31,3 +38,36 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 #endif
+
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button.init(action: action) {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height, alignment: .center)
+                .background(Color.orange)
+                .cornerRadius(size.width/2)
+        }
+    }
+}
+
+struct CalculatorButtonRow: View {
+    let row : [CalculatorButtonItem]
+    var body: some View {
+        HStack {
+            ForEach(row, id: \.self) { item in
+                CalculatorButton(title: item.title, size: item.size, backgroundColorName: item.backgroundColorName) {
+                    print("Button : \(item.title) ")
+                }}
+
+        }
+    }
+}
+
