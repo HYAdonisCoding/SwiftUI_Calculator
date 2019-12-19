3334cf7dbb573a7cead96d5ee8d957421d7fea63
曹弘扬 <296786475@qq.com>
2019-12-19 09:57:34 +0800

    Create PokeMaster Taget

---
+++ Adam_20191218_PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adam_20191218_PokeMaster
+//
+//  Created by Adonis_HongYang on 2019/12/18.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
